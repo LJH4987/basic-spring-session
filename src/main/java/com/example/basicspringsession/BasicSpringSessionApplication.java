395973e74653b8f5407c62419cf6c911b45d5b89
LJH <skRtl365@gmail.com>
2024-08-21 17:12:01 +0900

    init

---
+++ src/main/java/com/example/basicspringsession/BasicSpringSessionApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicspringsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringSessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/basicspringsession/BasicSpringSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicspringsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
